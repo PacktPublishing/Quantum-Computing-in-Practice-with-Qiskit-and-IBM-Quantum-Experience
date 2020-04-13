9c24a7c8cc7ce0412866eff2232f53d0d7603343
Hassi Norlen <hassi@norlens.net>
2020-04-13 16:34:46 +0200

    Chapter 3 recipes to import and export QASM

---
+++ ch3/ch3_r1_import_qasm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# For this simple recipe we will only need the QuantumCircuit method
+from qiskit import QuantumCircuit
+
+print("Ch 3: Moving between worlds 1")
+print("-----------------------------")
+
+# First we import the QASM string from IBM Qx
+qasm_string=input("Paste in a QASM string from IBM Qx (or enter the full path and file name of a .qasm file to import):\n")
+
+if qasm_string[-5:] == ".qasm":
+    # Create a quantum circuit from the file
+    circ=QuantumCircuit.from_qasm_file(qasm_string)
+else:
+    # Create a quantum circuit from the string
+    circ=QuantumCircuit.from_qasm_str(qasm_string)
+
+# Print the circuitCoin.qasm
+print("Imported quantum circuit")    
+print("------------------------")    
+print(circ)
---
+++ ch3/ch3_r2_export_qasm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# For this simple recipe we will only need the QuantumCircuit method
+from qiskit import QuantumCircuit
+
+print("Ch 3: Moving between worlds 2")
+print("-----------------------------")
+
+# First we create and print a random quantum circuit
+print("Random quantum circuit")
+print("----------------------\n")
+circ=random_circuit(2,2,measure=True)
+print(circ)
+
+
+# Next, we export the circuit as QASM code. If you include a filename you can also save the QASM code as a text file on your local machine
+print("\nOpenQASM code")
+print("-------------\n")
+
+circ.qasm(formatted=True, filename="Circuit.qasm")
+
+
